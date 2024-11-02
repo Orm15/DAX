8f07587ed9af4f69492102927694ad5466b72bd3
Victor <20100835@ue.edu.pe>
2024-11-01 22:46:48 -0500

    second commit

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 function copyToClipboard(text) {
     navigator.clipboard.writeText(text)
-        .then(() => {
-            alert("Código copiado al portapapeles");
-        })
-        .catch((error) => {
-            console.error("Error al copiar el texto: ", error);
-        });
 }
